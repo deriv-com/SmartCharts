3bcd58039acef6dd19eee8a052ffacff758ff273
Lee Zhen Yong <bruce@binary.com>
2018-01-03 17:22:52 +0800

    add hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const config = {
     entry: './src/index.js',
     output: {
+        publicPath: '/dist/',
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
