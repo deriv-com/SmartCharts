edd554725f15e61ab1d97295a8fc01c4984ac697
Lee Zhen Yong <bruce@binary.com>
2018-01-09 17:31:46 +0800

    simplify getParent implementation

--- src/components/ui/utils.js
+++ src/components/ui/utils.js
@@ -1,32 +1,22 @@
 // jQuery parents() vanilla alternative: https://stackoverflow.com/a/12981248/1471258
-export function getParents(el, parentSelector /* optional */) {
-    // If no parentSelector defined will bubble up all the way to *document*
-    if (parentSelector === undefined) {
-        parentSelector = document.body;
-    } else {
-        parentSelector = document.querySelector(parentSelector);
-        if (!parentSelector) return [];
-    }
+export function getParents(el, parentSelector = 'body') {
+    const parentElement = document.querySelector(parentSelector);
+
+    if (!parentElement) return [];
 
     let parents = [];
     let p = el.parentNode;
 
-    try {
-        while (p !== parentSelector) {
-            let o = p;
-            parents.push(o);
-            p = o.parentNode;
-        }
-    } catch (e) {
-        // if parentSelector do not exist in the hierarchy
-        if (e instanceof TypeError) {
-            return [];
-        }
+    while (p !== parentElement) {
+        // if parentElement is not in hierarchy
+        if (p === null) return [];
 
-        throw e;
+        parents.push(p);
+        p = p.parentNode;
     }
 
-    parents.push(parentSelector); // Push that parentSelector you wanted to stop at
+    // Include parent element in parents
+    parents.push(parentElement);
 
     return parents;
 }
