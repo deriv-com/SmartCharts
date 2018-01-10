0c8ba354a1e8f8f0558b9c3fed3c1f52263bf5e0
Lee Zhen Yong <bruce@binary.com>
2018-01-10 13:40:14 +0800

    separate css (binarychartiq.css) out of bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
 const webpack = require('webpack');
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const sassCssLoader = {
+    fallback: 'style-loader',
+    use: [
+        { loader: 'css-loader' },
+        { loader: 'sass-loader' },
+    ],
+};
+
+if (process.env.NODE_ENV) {
+    // set css-loader to minimize during production build
+    sassCssLoader.use[0].options = { minimize: true };
+}
 
 const config = {
     devtool: 'source-map',
@@ -11,56 +25,53 @@ const config = {
         filename: 'bundle.js',
     },
     module: {
-        rules: [{
-            test: /\.svg/,
-            use: {
-                loader: 'svg-url-loader',
+        rules: [
+            {
+                test: /\.(s*)css$/,
+                use: ['css-hot-loader'].concat(ExtractTextPlugin.extract(sassCssLoader)),
             },
-        },
-        {
-            test: [/\.(scss|css)$/],
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader',
-            ],
-        },
-        {
-            test: /\.(jpe?g|png|cur|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-            loader: 'base64-inline-loader',
-        },
-        { parser: { amd: false } },
-        {
-            test: /\.js$/,
-            exclude: [
-                /node_modules/,
-                /plugins\//,
-                /js\//,
-                /src\/components\//,
-            ],
-            loader: 'eslint-loader',
-            enforce: 'pre',
-            options: { fix: true },
-        },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        },
-        {
-            test: /\.html/,
-            use: 'raw-loader',
-        },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: ['url-loader'],
+            },
+            { parser: { amd: false } },
+            {
+                test: /\.js$/,
+                exclude: [
+                    /node_modules/,
+                    /plugins\//,
+                    /js\//,
+                    /src\/components\//,
+                ],
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                options: { fix: true },
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.html/,
+                use: 'raw-loader',
+            },
         ],
     },
     plugins: [
+        new ExtractTextPlugin({
+            filename: 'binarychartiq.css',
+        }),
         new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }),
-        // new webpack.optimize.UglifyJsPlugin(),
     ],
 };
 
 if (process.env.ANALYZE_BUNDLE) {
     config.plugins.push(new BundleAnalyzerPlugin());
+}
+
+if (process.env.NODE_ENV) {
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin());
 }
 
 
