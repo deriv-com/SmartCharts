cc6e09c662086e1adf1272e041581f1009a734b5
Lee Zhen Yong <bruce@binary.com>
2018-01-26 18:53:51 +0800

    upgrade splines.js + add spline implementation

--- src/components/ChartTypes.jsx
+++ src/components/ChartTypes.jsx
@@ -4,34 +4,66 @@ import contextAware from '../contextAware';
 import './ChartTypes.scss';
 
 const types = [
-    {id: 'mountain', name: 'Line'},
-    {id: 'line', name: 'Dot'},
-    {id: 'candle', name: 'Candle'},
-    {id: 'colored_bar', name: 'OHLC'},
-    {id: 'hollow_candle', name: 'Hollow Candle'},
+    {
+        id: 'mountain',
+        name: 'Line',
+    },
+    {
+        id: 'line',
+        name: 'Dot',
+    },
+    {
+        id: 'spline',
+        name: 'Spline',
+    },
+    {
+        id: 'candle',
+        name: 'Candle',
+    },
+    {
+        id: 'colored_bar',
+        name: 'OHLC',
+    },
+    {
+        id: 'hollow_candle',
+        name: 'Hollow Candle',
+    },
 ];
-types.forEach(type => {
+
+types.forEach((type) => {
     type.icon = `ciq-icon ciq-${type.id.replace('_', '-')}`;
 });
 
 class ChartTypes extends Component {
     state = { type: types[0] };
+
     onContextReady(context) {
         const type = types.find(t => t.id === context.stx.layout.chartType);
-        this.setState({type});
+        this.setState({ type });
 
         this._context = context;
     }
+
     setType(type) {
-        this._context.stx.setChartType(type.id);
-        this.setState({type});
+        const { stx } = this._context;
+        if (type.id === 'spline') {
+            // ChartIQ doesn't have a Spline chart type; the
+            // spline plot is created by setting the chart tension
+            stx.setChartType('mountain');
+            stx.chart.tension = 0.5;
+        } else {
+            stx.setChartType(type.id);
+            stx.chart.tension = 0;
+        }
+
+        this.setState({ type });
     }
 
     render() {
         return (
             <cq-menu class="ciq-menu ciq-display collapse ciq-chart-types">
-                <div className='ciq-title'>
-                    <span className={this.state.type.icon}></span>
+                <div className="ciq-title">
+                    <span className={this.state.type.icon} />
                     <span>{this.state.type.name}</span>
                 </div>
                 <cq-menu-dropdown>
@@ -41,7 +73,7 @@ class ChartTypes extends Component {
                             className="ciq-row"
                             key={idx}
                         >
-                            <span className={type.icon}></span>
+                            <span className={type.icon} />
                             <span>{type.name}</span>
                         </div>
                     ))}
@@ -51,5 +83,5 @@ class ChartTypes extends Component {
     }
 }
 
-export default  contextAware(ChartTypes);
+export default contextAware(ChartTypes);
 
