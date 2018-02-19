0d6c7d4ced17e4ef65c4413c1f705fc1b9bba61d
amin roosta <amin.roosta@outlook.com>
2018-02-19 14:51:40 +0800

    Use mobx in ChartTypes.jsx.

---
+++ src/store/ChartTypeStore.js
@@ -0,0 +1,41 @@
+import { observable, action, computed, autorunAsync } from 'mobx';
+
+export default class ChartTypeStore {
+    constructor(mainStore) {
+        this.mainStore = mainStore;
+        autorunAsync(this.onContextReady.bind(this));
+    }
+
+    get context() { return this.mainStore.chart.context; }
+    get stx() { return this.context.stx; }
+
+    onContextReady() {
+        if(this.context) {
+            const type = this.types.find(t => t.id === this.stx.layout.chartType);
+            this.type = type;
+        }
+    }
+
+    @action.bound setType(type) {
+        if (type.id === 'spline') {
+            // Spline is just a line with tension
+            this.stx.setChartType('mountain');
+            this.stx.chart.tension = 0.5;
+        } else {
+            this.stx.setChartType(type.id);
+            this.stx.chart.tension = 0;
+        }
+        this.type = type;
+    }
+
+    @observable types = [
+        { id: 'mountain', name: 'Line', },
+        { id: 'line', name: 'Dot', },
+        { id: 'spline', name: 'Spline', },
+        { id: 'candle', name: 'Candle', },
+        { id: 'colored_bar', name: 'OHLC', },
+        { id: 'hollow_candle', name: 'Hollow Candle', },
+    ];
+
+    @observable type = this.types[0];
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { observable } from 'mobx';
 import TimeperiodStore from './TimeperiodStore';
 import ChartStore from './ChartStore';
+import ChartTypeStore from './ChartTypeStore';
 
 export default class MainStore {
     @observable timeperiod = new TimeperiodStore(this);
     @observable chart = new ChartStore(this);
+    @observable chartType = new ChartTypeStore(this);
 }
