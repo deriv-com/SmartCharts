bcae7ea6fff8d18ca3cadd0a7c3305c7331c523c
amin roosta <amin.roosta@outlook.com>
2018-02-20 10:15:22 +0800

    use cursor: pointer

--- src/components/ChartControls.jsx
+++ src/components/ChartControls.jsx
@@ -1,30 +1,22 @@
-import $ from 'jquery';
 import React, { Component } from 'react';
-import contextAware from '../contextAware';
 import ChartTypes from './ChartTypes.jsx';
 import StudyLegend from './StudyLegend.jsx';
-import Timeperiod from './Timeperiod.jsx';
 import Comparison from './Comparison.jsx';
-import ChartSize from './ChartSize.jsx';
 import Views from './Views.jsx';
 import Toggles from './Toggles.jsx';
+import Timeperiod from './Timeperiod.jsx';
+import ChartSize from './ChartSize.jsx';
 
-class  ChartControls extends Component {
-    onContextReady(context) {
-    }
-    render() {
-        return (
-            <div id='chartControls'>
-                <ChartTypes />
-                <StudyLegend />
-                <Comparison />
-                <Views />
-                <Toggles />
-                <Timeperiod />
-                <ChartSize />
-            </div>
-        );
-    }
-}
+const ChartControls = () => (
+    <div id='chartControls'>
+        <ChartTypes />
+        <StudyLegend />
+        <Comparison />
+        <Views />
+        <Toggles />
+        <Timeperiod />
+        <ChartSize />
+    </div>
+);
 
-export default contextAware(ChartControls);
+export default ChartControls;
