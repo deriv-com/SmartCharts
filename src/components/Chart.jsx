7396858927ec7a7ae3c2a62e761ded1548e5603a
Lee Zhen Yong <bruce@binary.com>
2018-02-23 16:18:48 +0800

    place UI widgets outside of chartContainer so text inputs are interactable

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -61,6 +61,27 @@ class Chart extends Component {
                 <div className="ciq-chart-area">
                     <div className="ciq-chart">
                         <Toolbar />
+                        <div className="cq-top-ui-widgets">
+                            <ChartTitle />
+                            <cq-comparison >
+                                <cq-comparison-key
+                                    dangerouslySetInnerHTML={{
+                                        /* TODO: fix this */
+                                        __html: `
+                                <template cq-comparison-item>
+                                    <cq-comparison-item>
+                                        <cq-comparison-swatch></cq-comparison-swatch>
+                                        <cq-comparison-label>AAPL</cq-comparison-label>
+                                        <cq-comparison-price cq-animate></cq-comparison-price>
+                                        <cq-comparison-loader></cq-comparison-loader>
+                                        <div class="stx-btn-ico ciq-close"></div>
+                                    </cq-comparison-item>
+                                </template>
+                                    `,
+                                    }}
+                                />
+                            </cq-comparison>
+                        </div>
                         <div className="chartContainer primary">
 
                             <ChartControls />
@@ -75,27 +96,6 @@ class Chart extends Component {
                                     <stx-hu-tooltip-field-value />
                                 </stx-hu-tooltip-field>
                             </stx-hu-tooltip>
-                            <div className="cq-top-ui-widgets">
-                                <ChartTitle />
-                                <cq-comparison >
-                                    <cq-comparison-key
-                                        dangerouslySetInnerHTML={{
-                                            /* TODO: fix this */
-                                            __html: `
-                                <template cq-comparison-item>
-                                    <cq-comparison-item>
-                                        <cq-comparison-swatch></cq-comparison-swatch>
-                                        <cq-comparison-label>AAPL</cq-comparison-label>
-                                        <cq-comparison-price cq-animate></cq-comparison-price>
-                                        <cq-comparison-loader></cq-comparison-loader>
-                                        <div class="stx-btn-ico ciq-close"></div>
-                                    </cq-comparison-item>
-                                </template>
-                                    `,
-                                        }}
-                                    />
-                                </cq-comparison>
-                            </div>
                             <cq-hu-static>
                                 <div>
                                     <div>Price</div>
