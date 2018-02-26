026be2ed1cc0fd1834609a3fd9eac31639a3e6dc
amin roosta <amin.roosta@outlook.com>
2018-02-26 16:38:41 +0800

    cleanup jsx

--- src/components/AnimatedPrice.jsx
+++ src/components/AnimatedPrice.jsx
@@ -5,11 +5,12 @@ const AnimatedPrice = ({
     showStable,
     isIncrease,
     price,
-    className
+    className,
 }) => {
-    return (
-        <div className={`cq-animated-price ${className || ''} ${showStable ? 'cq-stable ' : (isIncrease ? 'cq-up' : 'cq-down')}`}>{price}</div>
-    );
+    const classes = `cq-animated-price ${className || ''} ${
+        showStable ? 'cq-stable ' : (isIncrease ? 'cq-up' : 'cq-down')
+    }`
+    return (<div className={classes}>{price}</div>);
 };
 
 export default AnimatedPrice;
