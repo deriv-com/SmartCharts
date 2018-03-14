578bbd347b1ae739df651078e1d1ce65bb09eeda
amin roosta <amin.roosta@outlook.com>
2018-03-14 11:07:57 +0800

    Remove ToggleStore

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import { observable } from 'mobx';
 import TimeperiodStore from './TimeperiodStore';
 import ChartStore from './ChartStore';
 import ChartTypeStore from './ChartTypeStore';
-import TogglesStore from './TogglesStore';
 import StudyLegendStore from './StudyLegendStore';
 import ComparisonStore from './ComparisonStore';
 import DrawToolsStore from './DrawToolsStore';
@@ -16,7 +15,6 @@ import CrosshairStore from './CrosshairStore';
 export default class MainStore {
     @observable chart = new ChartStore(this);
     @observable chartType = new ChartTypeStore(this);
-    @observable toggles = new TogglesStore(this);
     @observable studies = new StudyLegendStore(this);
     @observable comparison = new ComparisonStore(this);
     @observable drawTools = new DrawToolsStore(this);
