090a72b2e09b86476959a8776b9f671df44ea8c7
amin roosta <amin.roosta@outlook.com>
2018-03-19 15:15:24 +0800

    Show a notification when market is closed.

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -4,27 +4,33 @@ import { WarningIcon, CloseIcon, } from '../components/Icons.jsx';
 import '../../sass/_ciq-notification.scss';
 
 const Notification = ({
-    open,
-    setOpen,
-    text,
-    type, /* warning|info|error */
-}) => (
-    <div className={`cq-notification ${type} ${open ? 'open' : ''}`}>
-        <WarningIcon />
-        <div className='text'> {text} </div>
-        <CloseIcon
-            className='close-icon'
-            onClick={() => setOpen(false)}
-        />
-    </div>
-);
+    messages,
+    remove,
+}) => {
+    console.warn(messages);
+    return (
+        <div className='cq-notifications'>
+            {messages.map(({text, type, hide}, inx) => (
+                <div
+                    key={text}
+                    className={`notification ${type} ${hide ? 'hide' : ''}`}
+                >
+                    <WarningIcon />
+                    <div className='text'> {text} </div>
+                    <CloseIcon
+                        className='close-icon'
+                        onClick={() => remove(inx)}
+                    />
+                </div>
+            ))}
+        </div>
+    )
+}
 
 export default connect(
     ({notification: n}) => ({
-        open: n.open,
-        setOpen: n.setOpen,
-        text: n.text,
-        type: n.type
+        messages: n.messages.map(m => ({...m})),
+        remove: n.remove,
     })
 )(Notification);
 
--- src/store/NotificationStore.js
+++ src/store/NotificationStore.js
@@ -3,14 +3,31 @@ import { observable, action, computed, when } from 'mobx';
 export default class NotificationStore {
     constructor(mainStore) {
         this.mainStore = mainStore;
-        setTimeout(() => this.open = true, 5*1000);
+
+        setTimeout(() => {
+            this.messages.push( {
+                text:  'This product is currently in beta. Unexpected errors may occur.',
+                type: 'warning', // warning|info|error
+                hide: false,
+            });
+        }, 5*1000);
     }
 
-    @observable open = false;
-    @observable text = 'This product is currently in beta. Unexpected errors may occur.';
-    @observable type = 'warning'; // warning|info|error
+    @observable messages = [];
 
-    @action.bound setOpen(value) {
-        this.open = value;
+    @action.bound addWarning(text) {
+        this.messages = this.messages.filter(m => m.text !== text);
+        this.messages.push( {
+            text:  text,
+            type: 'warning',
+            hide: false,
+        });
+    }
+
+    @action.bound remove(inx) {
+        this.messages[inx].hide = true;
+        setTimeout(() => {
+            this.messages.splice(inx, 1);
+        }, 300);
     }
 }
