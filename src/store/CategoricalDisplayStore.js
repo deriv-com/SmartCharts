cb63397afbcdf1858fd17db13d032e0ab9626fe0
binarydevteam <shayan@binary.com>
2018-04-06 07:24:44 +0430

    UX Stuff: modifing search input,removing category section shadow,underlying assets subcategory,remove background highlight of category filter

--- src/store/CategoricalDisplayStore.js
+++ src/store/CategoricalDisplayStore.js
@@ -197,6 +197,15 @@ export default class CategoricalDisplayStore {
         }, 0);
     }
 
+    @action.bound clearFilterText() {
+        this.filterText = '';
+        this.searchInput.value = '';
+        setTimeout(() => {
+            this.scroll.refresh();
+            this.updateScrollSpy();
+        }, 0);
+    }
+
     @action.bound handleFilterClick(category) {
         const el = this.categoryElements[category.categoryId];
         if (el) {
@@ -281,6 +290,7 @@ export default class CategoricalDisplayStore {
     connect = connect(() => ({
         filterText: this.filterText,
         setFilterText: this.setFilterText,
+        clearFilterText: this.clearFilterText,
         filteredItems: this.filteredItems,
         getItemCount: this.getItemCount,
         setSearchInput: this.setSearchInput,
