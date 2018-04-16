2369d926a2ae441a6c05cfb1596fa0313caac75e
Lee Zhen Yong <bruce@binary.com>
2018-04-16 10:53:33 +0800

    remove past notifications after timeout

--- src/store/NotificationStore.js
+++ src/store/NotificationStore.js
@@ -1,5 +1,7 @@
 import { observable, action, computed, when } from 'mobx';
 
+let notificationId = 0;
+
 export default class NotificationStore {
     static get TYPE_ERROR()   { return 'error';   }
     static get TYPE_MESSAGE() { return 'message'; }
@@ -12,12 +14,26 @@ export default class NotificationStore {
 
     @observable messages = [];
 
-    @action.bound notify(text, type = NotificationStore.TYPE_WARNING) {
+    // Duration is in seconds; set to < 0 if you want the notification to remain indefinitely
+    @action.bound notify(text, type = NotificationStore.TYPE_WARNING, duration = 10) {
         this.messages = this.messages.filter(m => m.text !== text);
+        const id = notificationId++;
         this.messages.push( {
+            id,
             text,
             type,
             hide: false,
+        });
+        if (duration > 0) {
+            setTimeout(() => this.removeById(id), duration * 1000);
+        }
+    }
+
+    removeById(id) {
+        this.messages.map((msg, i) => {
+            if (msg.id === id) {
+                this.remove(i);
+            }
         });
     }
 
