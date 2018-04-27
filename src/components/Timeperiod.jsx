0e27081139b1a34036ddbdf598659f8c5dba9152
Mahboobeh Mohammadi <mahboub.meng@gmail.com>
2018-04-27 12:25:40 +0430

    Change time-period hove and active style to match with other chart-cotnrols

--- src/components/Timeperiod.jsx
+++ src/components/Timeperiod.jsx
@@ -12,15 +12,16 @@ const Timeperiod = ({
 }) => {
     return (
         <Menu
-            className="ciq-period"
-        >
+            className="ciq-period">
             <Menu.Title>
-                <span className="bt-priod" tooltip-title={t.translate("Time period")}>
-                    <span className="interval_display">{interval_display}</span>
-                    <span className="unit_display">{timeUnit_display}</span>
+                <div className="bt-priod">
+                    <span className="ic-priod">
+                        <span className="interval_display">{interval_display}</span>
+                        <span className="unit_display">{timeUnit_display}</span>
+                    </span>
                     <br/>
                     <span className='ic-subtitle'>{t.translate("Interval")}</span>
-                </span>
+                </div>
             </Menu.Title>
             <Menu.Body>
                 <div className="timeUnit">
