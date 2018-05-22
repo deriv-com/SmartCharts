469a673a6772796902b28023da0dc047297be403
Lee Zhen Yong <bruce@binary.com>
2018-05-22 13:32:55 +0800

    add aggregate chart types

--- src/store/ChartTypeStore.js
+++ src/store/ChartTypeStore.js
@@ -1,15 +1,63 @@
-import { observable, action, computed, when } from 'mobx';
+import { action, computed, observable, when } from 'mobx';
 import MenuStore from './MenuStore';
 import ListStore from './ListStore';
 import {
-    LineIcon,
-    DotIcon,
     BaseLineIcon,
     CandleIcon,
-    OHLCIcon,
+    DotIcon,
+    HeikinAshiIcon,
     HollowCandleIcon,
-    SplineIcon
+    KagiIcon,
+    LineBreakIcon,
+    LineIcon,
+    OHLCIcon,
+    PointFigureIcon,
+    RangeBarsIcon,
+    RenkoIcon,
+    SplineIcon,
 } from '../components/Icons.jsx';
+
+const aggregates = {
+    heikinashi: true,
+    kagi: {
+        title: t.translate('Kagi'),
+        inputs: [{
+            id: 'kagi',
+            title: t.translate('Reversal Percentage'),
+        }]
+    },
+    renko: {
+        title: t.translate('Renko'),
+        inputs: [{
+            id: 'renko',
+            title: t.translate('Range'),
+        }]
+    },
+    linebreak: {
+        title: t.translate('Line Break'),
+        inputs: [{
+            id: 'priceLines',
+            title: t.translate('Price Lines'),
+        }]
+    },
+    rangebars: {
+        title: t.translate('Range Bars'),
+        inputs: [{
+            id: 'range',
+            title: t.translate('Range'),
+        }]
+    },
+    pandf: {
+        title: t.translate('Point & Figure'),
+        inputs: [{
+            id: 'box',
+            title: t.translate('Box Size'),
+        }, {
+            id: 'reversal',
+            title: t.translate('Reversal'),
+        }]
+    }
+};
 
 export default class ChartTypeStore {
     constructor(mainStore) {
@@ -29,8 +77,14 @@ export default class ChartTypeStore {
     get stx() { return this.context.stx; }
 
     onContextReady = () => {
-        // We assume that if tension is set, spline is enabled
-        const chartType = this.stx.layout.tension ? 'spline' : this.stx.layout.chartType;
+        let chartType;
+        if (this.stx.layout.tension) { // We assume that if tension is set, spline is enabled
+            chartType = 'spline';
+        } else if (aggregates[this.stx.layout.aggregationType]) {
+            chartType = this.stx.layout.aggregationType;
+        } else {
+            chartType = this.stx.layout.chartType;
+        }
         const typeIdx = this.types.findIndex(t => t.id === chartType);
         this.list.selectedIdx = typeIdx;
         this.type = this.types[typeIdx];
@@ -47,7 +101,11 @@ export default class ChartTypeStore {
         } else {
             this.stx.chart.tension = 0;
             delete this.stx.layout.tension;
-            this.stx.setChartType(type.id);
+            if (aggregates[type.id]) {
+                this.stx.setAggregationType(type.id);
+            } else {
+                this.stx.setChartType(type.id);
+            }
         }
         this.type = type;
         this.menu.setOpen(false);
@@ -57,14 +115,20 @@ export default class ChartTypeStore {
         const isTickSelected = this.mainStore.timeperiod.timeUnit === 'tick';
 
         return [
-            { id: 'mountain',      text: t.translate('Line'),          disabled: false,          icon: LineIcon         },
-            { id: 'line',          text: t.translate('Dot'),           disabled: false,          icon: DotIcon          },
-            { id: 'colored_line',  text: t.translate('Colored Dot'),   disabled: false,          icon: DotIcon          },
-            { id: 'spline',        text: t.translate('Spline'),        disabled: false,          icon: SplineIcon       },
-            { id: 'baseline',      text: t.translate('Baseline'),      disabled: false,          icon: BaseLineIcon     },
-            { id: 'candle',        text: t.translate('Candle'),        disabled: isTickSelected, icon: CandleIcon       },
-            { id: 'colored_bar',   text: t.translate('OHLC'),          disabled: isTickSelected, icon: OHLCIcon         },
-            { id: 'hollow_candle', text: t.translate('Hollow Candle'), disabled: isTickSelected, icon: HollowCandleIcon },
+            { id: 'mountain',      text: t.translate('Line'),           disabled: false,          icon: LineIcon         },
+            { id: 'line',          text: t.translate('Dot'),            disabled: false,          icon: DotIcon          },
+            { id: 'colored_line',  text: t.translate('Colored Dot'),    disabled: false,          icon: DotIcon          },
+            { id: 'spline',        text: t.translate('Spline'),         disabled: false,          icon: SplineIcon       },
+            { id: 'baseline',      text: t.translate('Baseline'),       disabled: false,          icon: BaseLineIcon     },
+            { id: 'candle',        text: t.translate('Candle'),         disabled: isTickSelected, icon: CandleIcon       },
+            { id: 'colored_bar',   text: t.translate('OHLC'),           disabled: isTickSelected, icon: OHLCIcon         },
+            { id: 'hollow_candle', text: t.translate('Hollow Candle'),  disabled: isTickSelected, icon: HollowCandleIcon },
+            { id: 'heikinashi',    text: t.translate('Heikin Ashi'),    disabled: isTickSelected, icon: HeikinAshiIcon   },
+            { id: 'kagi',          text: t.translate('Kagi'),           disabled: isTickSelected, icon: KagiIcon         },
+            { id: 'linebreak',     text: t.translate('LineBreak'),      disabled: isTickSelected, icon: LineBreakIcon    },
+            { id: 'renko',         text: t.translate('Renko'),          disabled: isTickSelected, icon: RenkoIcon        },
+            { id: 'rangebars',     text: t.translate('Range Bars'),     disabled: isTickSelected, icon: RangeBarsIcon    },
+            { id: 'pandf',         text: t.translate('Point & Figure'), disabled: isTickSelected, icon: PointFigureIcon  },
         ].map(t => ({ ...t, active: t.id === this.type.id }));
     }
 
