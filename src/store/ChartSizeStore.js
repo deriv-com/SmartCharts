dd181a0e8b36044b538dc857a14ed9a0ed9811e4
Lee Zhen Yong <bruce@binary.com>
2018-05-24 18:08:42 +0800

    remove contextAware.js

---
+++ src/store/ChartSizeStore.js
@@ -0,0 +1,20 @@
+import { observable, when, action } from 'mobx';
+
+export default class ChartSizeStore {
+    @observable stx;
+
+    constructor(mainStore) {
+        this.mainStore = mainStore;
+        when(() => this.mainStore.chart.context, this.onContextReady);
+    }
+    onContextReady = () => this.stx = this.mainStore.chart.context.stx;
+
+    @action.bound zoomIn() {
+        if (this.stx) {this.stx.zoomIn();}
+    }
+
+    @action.bound zoomOut() {
+        if (this.stx) {this.stx.zoomOut();}
+    }
+}
+
