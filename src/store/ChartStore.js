7701e39b63ea80a483c563f2e56e561dac04ff57
Lee Zhen Yong <bruce@binary.com>
2018-06-07 10:59:09 +0800

    fix: symbolChange event should fire before tick_history finishes

--- src/store/ChartStore.js
+++ src/store/ChartStore.js
@@ -183,13 +183,7 @@ class ChartStore {
             this.saveLayout();
             this.chartPanelTop = holderStyle.top;
         });
-        stxx.addEventListener('symbolChange', (evt) => {
-            const isComparisonChart = evt.stx.chart.symbol !== evt.symbolObject.symbol;
-            if (this.onSymbolChange && !isComparisonChart) {
-                this.onSymbolChange(evt.symbolObject);
-            }
-            this.saveLayout();
-        });
+        stxx.addEventListener('symbolChange', this.saveLayout.bind(this));
         stxx.addEventListener('drawing', this.saveDrawings.bind(this));
         // stxx.addEventListener('newChart', () => { });
         stxx.addEventListener('preferences', this.savePreferences.bind(this));
@@ -288,6 +282,10 @@ class ChartStore {
         if (this.currentActiveSymbol
             && symbolObj.symbol === this.currentActiveSymbol.symbol) {
             return;
+        }
+
+        if (this.onSymbolChange) {
+            this.onSymbolChange(symbolObj);
         }
 
         this.loader.show();
