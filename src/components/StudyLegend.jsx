2988c407ca57801bd04e921312af15d4d70c3d90
shayan <shayan@binary.com>
2018-07-12 11:01:43 +0430

    fixing the problem of merging conflict

--- src/components/StudyLegend.jsx
+++ src/components/StudyLegend.jsx
@@ -1,51 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Menu from './Menu.jsx';
 import CategoricalDisplay from './CategoricalDisplay.jsx';
 import NotificationBadge from './NotificationBadge.jsx';
 import { connect } from '../store/Connect';
 import { IndicatorIcon } from './Icons.jsx';
 
-class StudyLegend extends Component {
-    componentWillUnmount() {
-        this.props.cleanUp();
-    }
-
-    render() {
-        const {
-            isOpened,
-            setOpen,
-            Menu,
-            menuOpen,
-            StudyCategoricalDisplay,
-            onCloseMenu,
-            isMobile,
-            activeStudiesNo,
-        } = this.props;
-
-        return (
-            <Menu
-                className="ciq-menu ciq-studies collapse"
-                isOpened={isOpened}
-                setOpen={setOpen}
-                isMobile={isMobile}
-            >
-                <Menu.Title>
-                    <IndicatorIcon
-                        className={`ic-icon-with-sub ${menuOpen ? 'active' : ''}`}
-                        tooltip-title={t.translate('Indicators')}
-                    />
-                    <NotificationBadge notificationCount={activeStudiesNo} />
-                </Menu.Title>
-                <Menu.Body>
-                    <StudyCategoricalDisplay
-                        dialogTitle={t.translate('Indicators')}
-                        closeMenu={() => onCloseMenu()}
-                    />
-                </Menu.Body>
-            </Menu>
-        );
-    }
-}
+const StudyLegend = ({
+    isOpened,
+    setOpen,
+    Menu,
+    menuOpen,
+    StudyCategoricalDisplay,
+    onCloseMenu,
+    isMobile,
+    activeStudiesNo,
+}) => (
+    <Menu
+        className="ciq-menu ciq-studies collapse"
+        isOpened={isOpened}
+        setOpen={setOpen}
+        isMobile={isMobile}
+    >
+        <Menu.Title>
+            <IndicatorIcon
+                className={`ic-icon-with-sub ${menuOpen ? 'active' : ''}`}
+                tooltip-title={t.translate('Indicators')}
+            />
+            <NotificationBadge notificationCount={activeStudiesNo} />
+        </Menu.Title>
+        <Menu.Body>
+            <StudyCategoricalDisplay
+                dialogTitle={t.translate('Indicators')}
+                closeMenu={() => onCloseMenu()}
+            />
+        </Menu.Body>
+    </Menu>
+);
 
 export default connect(({ studies: st }) => ({
     isOpened: st.open,
