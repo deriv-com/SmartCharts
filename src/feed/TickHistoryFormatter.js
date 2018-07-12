3f4d15f709ce2700a6a3150c1906cc07e1d27b05
Mahboobeh Mohammadi <mahboub.meng@gmail.com>
2018-07-12 08:36:58 +0430

    Fix test && refactor

--- src/feed/TickHistoryFormatter.js
+++ src/feed/TickHistoryFormatter.js
@@ -1,7 +1,7 @@
 export class TickHistoryFormatter {
-    static getUTCDate = function (date) {
-        const UTCdate = date.toISOString();
-        return UTCdate.substring(0, 10).concat(' ', UTCdate.substring(11, 19));
+    static getUTCDate(epoch) {
+        const UTCdate = new Date(epoch).toISOString();
+        return UTCdate.substring(0, 19);
     }
 
     static formatHistory(response) {
@@ -9,13 +9,13 @@ export class TickHistoryFormatter {
         if (history) {
             const { times, prices } = history;
             const quotes = prices.map((p, idx) => ({
-                Date: this.getUTCDate(new Date(+times[idx] * 1000)),
+                Date: this.getUTCDate(+times[idx] * 1000),
                 Close: +p,
             }));
             return quotes;
         } else if (candles) {
             const quotes = candles.map(c => ({
-                Date: this.getUTCDate(new Date(+c.epoch * 1000)),
+                Date: this.getUTCDate(+c.epoch * 1000),
                 Open: +c.open,
                 High: +c.high,
                 Low: +c.low,
@@ -29,12 +29,12 @@ export class TickHistoryFormatter {
         const { tick, ohlc } = response;
         if (tick) {
             return {
-                Date: this.getUTCDate(new Date(+tick.epoch * 1000)),
+                Date: this.getUTCDate(+tick.epoch * 1000),
                 Close: +tick.quote,
             };
         } else if (ohlc) {
             return {
-                Date: this.getUTCDate(new Date(+ohlc.open_time * 1000)),
+                Date: this.getUTCDate(+ohlc.open_time * 1000),
                 Open: +ohlc.open,
                 High: +ohlc.high,
                 Low: +ohlc.low,
