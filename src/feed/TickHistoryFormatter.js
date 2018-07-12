e7a78885ff6f9704cd51eb9d7922704208d7e4f9
Mahboobeh Mohammadi <mahboub.meng@gmail.com>
2018-07-12 11:08:24 +0430

    Refactor

--- src/feed/TickHistoryFormatter.js
+++ src/feed/TickHistoryFormatter.js
@@ -1,6 +1,6 @@
 export class TickHistoryFormatter {
     static getUTCDate(epoch) {
-        const UTCdate = new Date(epoch).toISOString();
+        const UTCdate = new Date(epoch * 1000).toISOString();
         return UTCdate.substring(0, 19);
     }
 
@@ -9,13 +9,13 @@ export class TickHistoryFormatter {
         if (history) {
             const { times, prices } = history;
             const quotes = prices.map((p, idx) => ({
-                Date: this.getUTCDate(+times[idx] * 1000),
+                Date: this.getUTCDate(+times[idx]),
                 Close: +p,
             }));
             return quotes;
         } else if (candles) {
             const quotes = candles.map(c => ({
-                Date: this.getUTCDate(+c.epoch * 1000),
+                Date: this.getUTCDate(+c.epoch),
                 Open: +c.open,
                 High: +c.high,
                 Low: +c.low,
@@ -29,12 +29,12 @@ export class TickHistoryFormatter {
         const { tick, ohlc } = response;
         if (tick) {
             return {
-                Date: this.getUTCDate(+tick.epoch * 1000),
+                Date: this.getUTCDate(+tick.epoch),
                 Close: +tick.quote,
             };
         } else if (ohlc) {
             return {
-                Date: this.getUTCDate(+ohlc.open_time * 1000),
+                Date: this.getUTCDate(+ohlc.open_time),
                 Open: +ohlc.open,
                 High: +ohlc.high,
                 Low: +ohlc.low,
