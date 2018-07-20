0c8f7c12dc6f79c11eecc5bdcb890e6568e12456
Lee Zhen Yong <bruce@binary.com>
2018-07-20 13:48:51 +0800

    fix views styling

--- src/components/Views.jsx
+++ src/components/Views.jsx
@@ -37,89 +37,87 @@ const Views = ({
     templateName,
     closeMenu,
 }) => (
-    <div className="ciq-views">
-        <Menu>
-            <Menu.Title className="cq-menu-btn">
-                <TemplateIcon
-                    className={`ic-icon-with-sub ${menuOpen ? 'active' : ''}`}
-                    tooltip-title={t.translate('Templates')}
+    <Menu className="ciq-views">
+        <Menu.Title className="cq-menu-btn">
+            <TemplateIcon
+                className={`ic-icon-with-sub ${menuOpen ? 'active' : ''}`}
+                tooltip-title={t.translate('Templates')}
+            />
+        </Menu.Title>
+        <Menu.Body>
+            <div className="title">
+                <div className="title-text">{t.translate('Templates')}</div>
+                <CloseIcon
+                    className="icon-close-menu"
+                    onClick={() => closeMenu()}
                 />
-            </Menu.Title>
-            <Menu.Body>
-                <div className="title">
-                    <div className="title-text">{t.translate('Templates')}</div>
-                    <CloseIcon
-                        className="icon-close-menu"
-                        onClick={() => closeMenu()}
-                    />
-                </div>
-                <div className="content">
-                    {
-                        currentRoute !== 'overwrite' ?
-                            <div>
-                                <div className="template-name">
-                                    {
-                                        currentRoute === 'add' ?
-                                            <span className="add">
-                                                <BackIcon onClick={cancel} />
-                                                <input
-                                                    ref={inputRef}
-                                                    className="view-input"
-                                                    value={templateName}
-                                                    placeholder={t.translate('Template name')}
-                                                    maxLength={20}
-                                                    onChange={onChange}
-                                                    onKeyUp={onSubmit}
-                                                />
-                                            </span>
-                                            : <span className="add-new" onClick={main}> {t.translate('Add New')} </span>
-                                    }
-                                    <span className={`icon ${!templateName ? 'hide' : ''}`}>
-                                        {
-                                            currentRoute === 'add'
-                                                ? <TickIcon className="tick-icon" onClick={add} />
-                                                : <AddIcon className="add-icon" onClick={main} />
-                                        }
-                                    </span>
-                                </div>
-                                <PerfectScrollbar className="ciq-list">
-                                    {
-                                        views.map((view, i) => (
-                                            <ViewItem
-                                                view={view}
-                                                key={i}
-                                                onClick={e => applyLayout(i, e)}
-                                                remove={e => remove(i, e)}
+            </div>
+            <div className="content">
+                {
+                    currentRoute !== 'overwrite' ?
+                        <div>
+                            <div className="template-name">
+                                {
+                                    currentRoute === 'add' ?
+                                        <span className="add">
+                                            <BackIcon onClick={cancel} />
+                                            <input
+                                                ref={inputRef}
+                                                className="view-input"
+                                                value={templateName}
+                                                placeholder={t.translate('Template name')}
+                                                maxLength={20}
+                                                onChange={onChange}
+                                                onKeyUp={onSubmit}
                                             />
-                                        ))
+                                        </span>
+                                        : <span className="add-new" onClick={main}> {t.translate('Add New')} </span>
+                                }
+                                <span className={`icon ${!templateName ? 'hide' : ''}`}>
+                                    {
+                                        currentRoute === 'add'
+                                            ? <TickIcon className="tick-icon" onClick={add} />
+                                            : <AddIcon className="add-icon" onClick={main} />
                                     }
-                                </PerfectScrollbar >
+                                </span>
                             </div>
-                            :
-                            <div className="ovrwrit-alrt">
-                                <div className="ovrwrit-alrt-title">
-                                    <alertIconMap.warning />
-                                    <span>
-                                        {templateName + t.translate(' is already exist.')}
-                                    </span>
-                                    <span>
-                                        {t.translate('Would you like to overwrite it?')}
-                                    </span>
+                            <PerfectScrollbar className="ciq-list">
+                                {
+                                    views.map((view, i) => (
+                                        <ViewItem
+                                            view={view}
+                                            key={i}
+                                            onClick={e => applyLayout(i, e)}
+                                            remove={e => remove(i, e)}
+                                        />
+                                    ))
+                                }
+                            </PerfectScrollbar >
+                        </div>
+                        :
+                        <div className="ovrwrit-alrt">
+                            <div className="ovrwrit-alrt-title">
+                                <alertIconMap.warning />
+                                <span>
+                                    {templateName + t.translate(' is already exist.')}
+                                </span>
+                                <span>
+                                    {t.translate('Would you like to overwrite it?')}
+                                </span>
+                            </div>
+                            <div className="ovrwrit-alrt-buttons">
+                                <div onClick={main}>
+                                    {t.translate('CANCEL')}
                                 </div>
-                                <div className="ovrwrit-alrt-buttons">
-                                    <div onClick={main}>
-                                        {t.translate('CANCEL')}
-                                    </div>
-                                    <div onClick={overwrite}>
-                                        {t.translate('OVERWRITE')}
-                                    </div>
+                                <div onClick={overwrite}>
+                                    {t.translate('OVERWRITE')}
                                 </div>
                             </div>
-                    }
-                </div>
-            </Menu.Body>
-        </Menu>
-    </div>
+                        </div>
+                }
+            </div>
+        </Menu.Body>
+    </Menu>
 );
 
 export default connect(({ view: s }) => ({
