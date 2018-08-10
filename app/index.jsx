b34109c5d680e85753fac63d11e8f1bc16bd11d9
shayan <shayan@binary.com>
2018-08-10 12:21:59 +0430

    make spot centerize and also sure to be circle

--- app/index.jsx
+++ app/index.jsx
@@ -76,6 +76,7 @@ const requestForget = streamManager.forget.bind(streamManager);
 
 
 class App extends Component {
+    startingLanguage = 'en';
     constructor(props) {
         super(props);
         this.notifier = new ChartNotifier();
@@ -107,7 +108,6 @@ class App extends Component {
         }
     };
 
-    startingLanguage = 'en';
     render() {
         const { settings, isConnectionOpened } = this.state;
 
