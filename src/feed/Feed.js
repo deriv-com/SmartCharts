260f2798569c55b545f1905e729a24ef25f611a4
Lee Zhen Yong <bruce@binary.com>
2018-08-20 15:34:04 +0800

    minor refactor

--- src/feed/Feed.js
+++ src/feed/Feed.js
@@ -11,18 +11,18 @@ class Feed {
     get startEpoch() { return this._mainStore.state.startEpoch; }
     get endEpoch() { return this._mainStore.state.endEpoch; }
     get context() { return this._mainStore.chart.context; }
+    _activeStreams = {};
+    _lastStreamEpoch = {};
+    _isConnectionOpened = true;
 
     constructor(binaryApi, stx, mainStore) {
         this._stx = stx;
         this._binaryApi = binaryApi;
-        this._activeStreams = {};
-        this._lastStreamEpoch = {};
         this._mainStore = mainStore;
         reaction(() => mainStore.state.isConnectionOpened, this.onConnectionChanged.bind(this));
         when(() => this.context, this.onContextReady);
 
         this._emitter = new EventEmitter({ emitDelay: 0 });
-        this._isConnectionOpened = true;
     }
 
     onContextReady = () => {
