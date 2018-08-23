49e1deceb74a1d9582d29db313d7ce99bd8ccff4
shayanbinary <38674882+shayanbinary@users.noreply.github.com>
2018-08-23 11:54:01 +0430

    Chart Spot Animation (#415)
    * use css animation beside of canvas
    
    * clean code
    
    * clean code
    
    * cleanup code
    
    * cleanup for stylelint
    
    * adding mixin fr keyframe
    
    * force spot animation to display in realtime interval 1 second
    
    * cleanup code - remove unneccessary
    
    * remove force of animation for inerval 1 second
    
    * try to fix wobbly effetc
    
    * hide animation if there isn't any move
    
    * POC
    
    * add controller for browser resizing
    
    * removing spot on some chart type
    
    * fix module error, and also convert x,y from integer to double
    
    * make spot centerize and also sure to be circle
    
    * clean up code
    
    * use px beside % for border-radius
    
    * make spot to be circle
    
    * remvoe white space from spot
    
    * attempt to reduce wobbly-ness
    
    * double the animation speed

---
+++ src/components/CurrentSpot.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from '../store/Connect';
+
+const CurrentSpot = ({
+    left,
+    top,
+    show,
+}) => (show
+    && (
+        <div>
+            <span
+                className="cq-spot"
+                style={{ top, left }}
+            />
+        </div>
+    )
+);
+
+export default connect(({ currentSpot: cs }) => ({
+    left: cs.left,
+    top: cs.top,
+    show: cs.show,
+}))(CurrentSpot);
--- src/components/ui/Animation.js
+++ src/components/ui/Animation.js
@@ -40,13 +40,8 @@ CIQ.Animation = function (stx, animationParameters, easeMachine) {
     animationParameters = CIQ.extend(params, animationParameters);
 
     if (params.tension) stx.chart.tension = animationParameters.tension;
-    stx.tickAnimator = easeMachine || new CIQ.EaseMachine(Math.easeOutCubic, 1000);
-    let scrollAnimator = new CIQ.EaseMachine(Math.easeInOutCubic, 1000);
-
-    let flashingColors = ['#0298d3', '#19bcfc', '#5dcffc', '#9ee3ff'];
-    let flashingColorIndex = 0;
-    let flashingColorThrottle = 20;
-    let flashingColorThrottleCounter = 0;
+    stx.tickAnimator = easeMachine || new CIQ.EaseMachine(Math.easeOutCubic, 500);
+    let scrollAnimator = new CIQ.EaseMachine(Math.easeInOutCubic, 500);
 
     let filterSession = false;
     let nextBoundary = null;
@@ -252,36 +247,6 @@ CIQ.Animation = function (stx, animationParameters, easeMachine) {
                 lineOffset: 0,
             });
             return true; // bypass default behavior in favor of animation
-        }
-    });
-
-    stx.append('draw', function () {
-        if (filterSession) { return; }
-        if (this.chart.dataSet && this.chart.dataSet.length && this.mainSeriesRenderer && this.mainSeriesRenderer.supportsAnimation) {
-            if (flashingColorThrottleCounter % flashingColorThrottle === 0) {
-                flashingColorIndex++;
-                flashingColorThrottleCounter = 0;
-            }
-            flashingColorThrottleCounter++;
-
-            let context = this.chart.context;
-            let panel = this.chart.panel;
-            let currentQuote = this.currentQuote();
-            if (!currentQuote) { return; }
-            let price = currentQuote.Close;
-            let x = this.pixelFromTick(currentQuote.tick, this.chart);
-            if (this.chart.lastTickOffset) { x += this.chart.lastTickOffset; }
-            let y = this.pixelFromPrice(price, panel);
-            if (this.chart.yAxis.left > x
-                && this.chart.yAxis.top <= y
-                && this.chart.yAxis.bottom >= y) {
-                if (flashingColorIndex >= flashingColors.length) { flashingColorIndex = 0; }
-                context.beginPath();
-                context.moveTo(x, y);
-                context.arc(x, y, 2 + flashingColorIndex * 1.07, 0, Math.PI * 2, false);
-                context.fillStyle = flashingColors[flashingColorIndex];
-                context.fill();
-            }
         }
     });
 };
