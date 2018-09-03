54874a13355de3f70bbbdeffddb0b021bdc3143d
shayan <shayan@binary.com>
2018-09-03 05:17:17 +0430

    add base pwa config

---
+++ sw.js
@@ -0,0 +1,38 @@
+var cacheName = 'SmartCharts';
+var filesToCache = [
+  './index.html',
+  './dist/smartcharts.css',
+  './dist/babel-polyfill.min.js',
+  './dist/chartiq.min.js',
+  './dist/react.js',
+  './dist/react-dom.js',
+  './dist/react-transition-group.js',
+  './dist/mobx.js',
+  './dist/mobx-react.js',
+  './dist/smartcharts.js'
+];
+
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event){
+  console.log('activate');
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event){
+  console.log('fetch');
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(function(response){
+      return response || fetch(event.request);
+    })
+  );
+});
