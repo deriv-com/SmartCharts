82f21eeb9cdaf6e682579df40556e0165a783d64
mahboobm <mahboub.meng@gmail.com>
2018-09-24 10:03:38 +0330

    Fix test issues

--- webpack.config.js
+++ webpack.config.js
@@ -69,8 +69,8 @@ const config = {
                 test: /\.(js|jsx)$/,
                 exclude: [
                     /node_modules/,
-                    /chartiq\//,
-                    /scripts\//,
+                    /\\chartiq/,
+                    /\\scripts/,
                 ],
                 loader: 'eslint-loader',
                 enforce: 'pre',
