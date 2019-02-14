a0fcbd7b37820f70e5f885731a7000e45b1738be
Mahboobeh <mahboobeh@Mahboobehs-MacBook-Pro.local>
2019-02-14 14:08:16 +0800

    Remove tail at the right side of current spot

--- src/store/ChartStore.js
+++ src/store/ChartStore.js
@@ -260,8 +260,9 @@ class ChartStore {
                 const panel = chart.panel;
                 const currentQuote = this.currentQuote();
                 const x = this.pixelFromTick(currentQuote.tick, chart) + (chart.lastTickOffset || 0);
-                // Change the panel.left to current spot position for drawing current price line
+                // Change the panel position to current spot position for drawing current price line
                 panel.left = x;
+                panel.right -= 16;
             }
         });
         CIQ.ChartEngine.prototype.append('drawCurrentHR', function () {
@@ -270,8 +271,9 @@ class ChartStore {
             for (const chartName in this.charts) {
                 const chart = this.charts[chartName];
                 const panel = chart.panel;
-                // Set the panel.left to its original value
+                // Set the panel position to its original value
                 panel.left = 0;
+                panel.right += 16;
             }
         });
 
