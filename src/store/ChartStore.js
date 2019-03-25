5323b743665eb03fb5551931af18d0488038d0c2
Mahboobeh <mahboobeh@Mahboobehs-MacBook-Pro.local>
2019-03-25 17:17:00 +0800

    bug fix

--- src/store/ChartStore.js
+++ src/store/ChartStore.js
@@ -585,7 +585,7 @@ class ChartStore {
         }
 
         const { chartType: chartTypeStore } = this.mainStore;
-        if (chartTypeStore.chartTypeProp === undefined) {
+        if (!this.mainStore.chartType.type && !chartTypeStore.chartTypeProp) {
             this.contextPromise.then(() => {
                 const isTick = this.stxx.layout.timeUnit === 'second';
                 const isCandle = chartTypeStore.isCandle;
