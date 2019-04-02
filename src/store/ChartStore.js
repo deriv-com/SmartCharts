fb342db91ab97ed4f8ae28f08fc89bd2e993edad
Mahboobeh <mahboobeh@Mahboobehs-MacBook-Pro.local>
2019-04-02 16:43:45 +0800

    Keep charttype after changing symbols

--- src/store/ChartStore.js
+++ src/store/ChartStore.js
@@ -585,18 +585,14 @@ class ChartStore {
         }
 
         const { chartType: chartTypeStore } = this.mainStore;
-        if (chartTypeStore.chartTypeProp === undefined) {
-            this.contextPromise.then(() => {
-                const isTick = this.stxx.layout.timeUnit === 'second';
-                const isCandle = chartTypeStore.isCandle;
-                if (isCandle && isTick) {
-                    // Tick charts cannot be represented with candles
-                    chartTypeStore.setType('mountain');
-                } else if (!isTick && !isCandle) {
-                    chartTypeStore.setType('candle');
-                }
-            });
-        }
+        this.contextPromise.then(() => {
+            const isTick = this.stxx.layout.timeUnit === 'second';
+            const isCandle = chartTypeStore.isCandle;
+            if (isCandle && isTick) {
+                // Tick charts cannot be represented with candles
+                chartTypeStore.setType('mountain');
+            }
+        });
     }
 
     // Calling newChart with symbolObj as undefined refreshes the chart
