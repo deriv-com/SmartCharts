516f6bf955ffba04d13f292af9e17e92e562c1ea
Kelly Anyi <kelly@binary.com>
2019-05-08 14:29:36 +0800

    set bottom value for start/end markers if there's a bottom widget

--- src/store/MarkerStore.js
+++ src/store/MarkerStore.js
@@ -160,7 +160,8 @@ export default class MarkerStore {
 
         // Y axis positioning logic
         if (this.yPositioner.toLowerCase() === 'none') {
-            this.bottom = undefined;
+            // set the bottom value if there's a bottomWidget, else set it to undefined
+            this.bottom = this.stx.chart.yAxis.initialMarginBottom !== 50 ? 130 : undefined;
             this.showMarker();
             return;
         }
