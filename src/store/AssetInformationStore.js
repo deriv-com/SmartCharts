3759af2aa977b8c86aa2011f75ca7ec978ccbffa
amin roosta <amin.roosta@outlook.com>
2019-07-01 15:04:54 +0800

    make asset-information .3ms faster

--- src/store/AssetInformationStore.js
+++ src/store/AssetInformationStore.js
@@ -20,35 +20,67 @@ export default class AssetInformationStore {
         this.stx.prepend('headsUpHR', this.update);
     };
 
+    _container = null;
+    _spot = null;
+    _open = null;
+    _close = null;
+    _high = null;
+    _low  = null;
+
+
     update = () => {
-        const container = this.context.topNode.querySelector('.ciq-asset-information');
+        // do not query dom on each frame
+        if (!this._container) {
+            this._container = this.context.topNode.querySelector('.ciq-asset-information');
+
+            this._spot = this._container.querySelector('.ciq-ai-spot');
+            this._open = this._container.querySelector('.ciq-ai-open');
+            this._close = this._container.querySelector('.ciq-ai-close');
+            this._high = this._container.querySelector('.ciq-ai-high');
+            this._low = this._container.querySelector('.ciq-ai-low');
+        }
+        const container = this._container;
+
         if (!this.visible) {
-            container.innerHTML = '';
+            container.style.visibility = 'hidden';
             return;
         }
 
         const bar = this.stx.barFromPixel(this.stx.cx);
         const prices = this.stx.chart.xaxis[bar];
 
-        if (!prices) { return; }
+        if (!prices) {
+            return;
+        }
+
 
         const {
             Open, High, Low, Close,
         } = prices.data || { };
-        const open = (Open && !this.isTick) ? Open.toFixed(this.decimalPlaces) : null;
-        const high = (High && !this.isTick) ? High.toFixed(this.decimalPlaces) : null;
-        const low = (Low && !this.isTick) ? Low.toFixed(this.decimalPlaces) : null;
-        const close = (Close && !this.isTick) ? Close.toFixed(this.decimalPlaces) : null;
-        const price = Close ? Close.toFixed(this.decimalPlaces) : null;
 
-        // The value of the div is set by VanillaJS to improve the preformace of the chart.
-        if (container) {
-            container.innerHTML = `
-                ${price ? `<div> <div>${t.translate('SPOT')}:</div> <div>${price}</div> </div>`  : ''}
-                ${open  ? `<div> <div>${t.translate('OPEN')}:</div> <div>${open}</div> </div>`   : ''}
-                ${close ? `<div> <div>${t.translate('CLOSE')}:</div> <div>${close}</div> </div>` : ''}
-                ${high  ? `<div> <div>${t.translate('HIGH')}:</div> <div>${high}</div> </div>`   : ''}
-                ${low   ? `<div> <div>${t.translate('LOW')}:</div> <div>${low}</div> </div>`     : ''}`;
+        if (!Open && !High && !Low && !Close) {
+            container.style.visibility = 'hidden';
+            return;
+        }
+
+        container.style.visibility = 'visible';
+
+        if (this.isTick) {
+            const children = container.children;
+            const visibility = this.isTick ? 'hidden' : 'visible';
+
+            for (let idx = 1; idx < children.length; ++idx) {
+                children[idx].style.visibility = visibility;
+            }
+        }
+
+
+        if (this._container) {
+            this._open.textContent = (Open && !this.isTick) ? Open.toFixed(this.decimalPlaces) : '';
+            this._high.textContent = (High && !this.isTick) ? High.toFixed(this.decimalPlaces) : '';
+            this._low.textContent = (Low && !this.isTick) ? Low.toFixed(this.decimalPlaces) : '';
+            this._close.textContent = (Close && !this.isTick) ? Close.toFixed(this.decimalPlaces) : '';
+            this._spot.textContent = Close ? Close.toFixed(this.decimalPlaces) : null;
         }
     }
 }
