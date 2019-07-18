522aeb78063ea8a7cf3891a8d3f7f2b895336ea4
Kelly Anyi <kelly@binary.com>
2019-07-18 13:47:03 +0800

    slow down mouse wheel

--- src/store/PaginationLoaderStore.js
+++ src/store/PaginationLoaderStore.js
@@ -14,18 +14,22 @@ class PaginationLoaderStore {
     }
 
     onContextReady = () => {
-        this.feed.onStartPagination(this.setOnPagination.bind(this));
-        this.feed.onPagination(this.setOnPagination.bind(this));
+        this.feed.onStartPagination(this.setOnPagination);
+        this.feed.onPagination(this.setOnPagination);
 
-        // this injection will stop the swiping and mouse wheel
-        // operations when isOnPagination is true
+        // this injection will slows down the swiping and mouse wheel operations
+        // by ignoring requests that are too close from one another or
+        // when isOnPagination is true
         this.stx.prepend('mouseWheel', this.onMouseWheel);
     };
 
     onMouseWheel = (e) => {
         e.preventDefault();
-        if (this.isOnPagination) return true; // skip swiping
-        // continue swiping
+        let diff = null;
+        const timeLimit = 40;
+        if (this.stx.lastMouseWheelEvent) diff = Date.now() - this.stx.lastMouseWheelEvent;
+        if (diff && diff < timeLimit || this.isOnPagination) return true; // skip swiping
+        return false; // continue swiping
     }
 
     setRef = (ref) => {
@@ -35,7 +39,7 @@ class PaginationLoaderStore {
         }
     }
 
-    setOnPagination({ end }) {
+    setOnPagination = ({ end }) => {
         if (!this.ref) { return; }
         this.isOnPagination     = !this.isOnPagination;
         this.paginationEndEpoch = this.isOnPagination ? end : null;
