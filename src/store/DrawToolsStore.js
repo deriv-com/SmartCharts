dd38358ae0691a393955b3628ea28d19796df7af
nazaninreihani <nazanin.reyh@gmail.com>
2019-07-18 18:25:24 -0400

    fix error of drawing tools for continues draw tool

--- src/store/DrawToolsStore.js
+++ src/store/DrawToolsStore.js
@@ -32,11 +32,13 @@ export default class DrawToolsStore {
     get stx() { return this.context.stx; }
 
     activeDrawing = null;
+    isContinuous = false;
 
-    drawToolsItems = []
+    drawToolsItems = [];
 
     onContextReady = () => {
         document.addEventListener('keydown', this.closeOnEscape, false);
+        this.stx.addEventListener('drawing', this.noTool);
         this.stx.prepend('deleteHighlighted', this.onDeleteHighlighted);
         this.drawToolsItems = [
             { id: 'annotation',  text: t.translate('Annotation') },
@@ -123,9 +125,11 @@ export default class DrawToolsStore {
     }
 
     noTool = () => {
-        if (this.menu.open && this.context) {
+        const count = this.stx.drawingObjects.length;
+        if ((this.menu.open && this.context) || (!this.isContinuous && this._pervDrawingObjectCount !== count)) {
             this.stx.changeVectorType('');
         }
+        this._pervDrawingObjectCount = count;
     };
 
     @action.bound clearAll() {
@@ -134,10 +138,14 @@ export default class DrawToolsStore {
     }
 
     @action.bound selectTool(id) {
+        this.isContinuous = false;
         logEvent(LogCategories.ChartControl, LogActions.DrawTools, `Add ${id}`);
         const stx = this.context.stx;
         stx.clearMeasure(); // TODO remove this line
         stx.changeVectorType(id);
+        if (id === 'continuous') {
+            this.isContinuous = true;
+        }
         this.menu.setOpen(false);
         // let drawingParameters = CIQ.Drawing.getDrawingParameters(stx, id);
     }
