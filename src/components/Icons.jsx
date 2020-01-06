29f8aa495d1c75acb0e595880764bf3055542b8a
balakrishna-binary <56330681+balakrishna-binary@users.noreply.github.com>
2020-01-06 17:10:37 +0800

    Bala|Kelly/Multiplier (#905)
    * title tag in barrier & hideOffscreenLines check
    
    * hidePriceLabel flag and refactor title tag
    
    * new barrier design
    
    * refactor
    
    * bg gradient and rounded edges
    
    * made offScreen as observable to show/hide dragline when yAxis is scaled
    
    * added hideBarrierLine config
    
    * barrier arrows & refactoring
    
    * eslint
    
    * price line arrow
    
    * eslint
    
    * fix arrow height and start marker floating
    
    * hideOffscreenBarrier
    
    * hideOffscreenBarrier observable
    
    * overlap check
    
    * loop fix
    
    * offScreenDirection
    
    * markdown barrier api update
    
    * typo fix
    
    * removed computed decorator

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -68,6 +68,9 @@ import Scale from '../../sass/icons/navigation-widgets/ic-scale-full.svg';
 import Zoomin from '../../sass/icons/navigation-widgets/ic-zoomin.svg';
 import Zoomout from '../../sass/icons/navigation-widgets/ic-zoomout.svg';
 
+import ArrowGreen from '../../sass/icons/arrows/ic-arrow-green.svg';
+import ArrowOrange from '../../sass/icons/arrows/ic-arrow-orange.svg';
+
 import SymbolPlaceholder from '../../sass/icons/placeholder/ic-placeholder.svg';
 import AUD from '../../sass/icons/flags/aud.svg';
 import CAD from '../../sass/icons/flags/cad.svg';
@@ -200,6 +203,9 @@ export const ZoominIcon = Wrapper(Zoomin);
 export const ZoomoutIcon = Wrapper(Zoomout);
 
 export const MetalIcon = Wrapper(Metal);
+
+export const ArrowGreenIcon = Wrapper(ArrowGreen);
+export const ArrowOrangeIcon = Wrapper(ArrowOrange);
 
 export const alertIconMap = {
     info: Wrapper(Info),
