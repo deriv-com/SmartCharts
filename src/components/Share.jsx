461bd4b01a7221fd63f43821d0aafa5864a22b69
shayan-binary-pr <shayan-binary-pr@protonmail.com>
2020-01-07 07:05:45 +0330

    set option for new deisgn and old design

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -22,11 +22,12 @@ const Share = ({
     downloadCSV,
     downloadPNG,
     isLoadingPNG,
+    newMode,
 }) => (
     <Dialog
         className="cq-download-menu"
         title={t.translate('Download')}
-        newStyle
+        newStyle={newMode}
     >
         <Dialog.Title>
             <DownloadIcon
@@ -35,23 +36,51 @@ const Share = ({
             />
         </Dialog.Title>
         <Dialog.Body>
-            <div className="cq-download">
-                <div
-                    className={`cq-download__item ${isLoadingPNG ? 'cq-download__item--loading' : ''}`}
-                    onClick={downloadPNG}
-                >
-                    <PngIcon />
-                    <span className="cq-download__item__label"> {t.translate('PNG')} </span>
-                    {isLoadingPNG && <Loading />}
+            {newMode ? (
+                <div className="cq-download">
+                    <div
+                        className={`cq-download__item ${isLoadingPNG ? 'cq-download__item--loading' : ''}`}
+                        onClick={downloadPNG}
+                    >
+                        <PngIcon />
+                        <span className="cq-download__item__label"> {t.translate('PNG')} </span>
+                        {isLoadingPNG && <Loading />}
+                    </div>
+                    <div
+                        className="cq-download__item"
+                        onClick={downloadCSV}
+                    >
+                        <CsvIcon />
+                        <span className="cq-download__item__label"> {t.translate('CSV')}</span>
+                    </div>
                 </div>
-                <div
-                    className="cq-download__item"
-                    onClick={downloadCSV}
-                >
-                    <CsvIcon />
-                    <span className="cq-download__item__label"> {t.translate('CSV')}</span>
+            ) : (
+                <div className="body">
+                    <div className="content">
+                        <div className="ciq-list ciq-list-download">
+                            <div
+                                className="ciq-list-item"
+                                onClick={downloadPNG}
+                            >
+                                <span className="ciq-icon-text">
+                                    {t.translate('Download as PNG')}
+                                    {isLoadingPNG && <span className="cq-loading" />}
+                                </span>
+                                <PngIcon />
+                            </div>
+                            <div
+                                className="ciq-list-item"
+                                onClick={downloadCSV}
+                            >
+                                <span className="ciq-icon-text">
+                                    {t.translate('Download as CSV')}
+                                </span>
+                                <CsvIcon />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            )}
         </Dialog.Body>
     </Dialog>
 );
