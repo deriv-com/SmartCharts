bafe500fe5aebe4d9d23aeeedb85eb75b2b65fbb
shayan-binary-pr <shayan-binary-pr@protonmail.com>
2020-01-28 07:37:26 +0330

    Rename Line to Area

--- src/store/ChartTypeStore.js
+++ src/store/ChartTypeStore.js
@@ -15,7 +15,7 @@ import { logEvent, LogCategories, LogActions } from  '../utils/ga';
 
 function getChartTypes() {
     return [
-        { id: 'mountain',      text: t.translate('Line'),   candleOnly: false, icon: TypeAreaIcon   },
+        { id: 'mountain',      text: t.translate('Area'),   candleOnly: false, icon: TypeAreaIcon   },
         { id: 'candle',        text: t.translate('Candle'), candleOnly: true,  icon: TypeCandleIcon },
         { id: 'hollow_candle', text: t.translate('Hollow'), candleOnly: true,  icon: TypeHollowIcon },
         { id: 'colored_bar',   text: t.translate('OHLC'),   candleOnly: true,  icon: TypeOhlcIcon   },
