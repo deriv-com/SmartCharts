e88232a070841196d2aad7ad8f699e329ae0d49f
shayan <shayan@firstsource.tech>
2020-07-27 11:10:47 +0430

    change resource script hashing

--- scripts/update-hashes.js
+++ scripts/update-hashes.js
@@ -5,10 +5,11 @@ const HASH_LENGTH = 8;
 
 let html = fs.readFileSync('index.html', 'utf-8');
 const files = html.match(/(\.\/dist\/[a-zA-Z.-]+)/g);
-files.forEach((file) => {
-    const hash = md5File.sync(file).substring(0, HASH_LENGTH);
-    html = html.replace(file, 'PLACEHOLDER');
-    html = html.replace(/PLACEHOLDER[^"]*/, `${file}?${hash}`);
+files.forEach((filePath) => {
+    const hash = md5File.sync(filePath).substring(0, HASH_LENGTH);
+    const newFilePath = filePath.replace('.css', `-${hash}.css`).replace('.js', `-${hash}.js`)
+    fs.renameSync(filePath, newFilePath);
+    html = html.replace(filePath, newFilePath);
 });
 
 fs.writeFileSync('index.html', html, 'utf-8');
