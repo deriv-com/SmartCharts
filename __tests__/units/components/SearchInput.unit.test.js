276e10ed4bbd3b6208661089b4d289755dc64b19
shayan <shayan@firstsource.tech>
2021-02-22 05:53:25 +0330

    add more test to search input

--- __tests__/units/components/SearchInput.unit.test.js
+++ __tests__/units/components/SearchInput.unit.test.js
@@ -4,8 +4,27 @@ import SearchInput from '../../../src/components/SearchInput'
 
 describe('SearchInput', function() {
     it('render correctly', () => {
-        const wrapper = shallow(<SearchInput />)
+        const wrapper = shallow(<SearchInput placeholder="sample-placeholder" value="sample" searchInputClassName="sample-class" />)
+        expect(wrapper.find('.sc-search-input').length).toBe(1);
+        expect(wrapper.find('.sc-search-input').hasClass('active')).toBe(true);
+        expect(wrapper.find('.sc-search-input > input').hasClass('sample-class')).toBe(true);
+        expect(wrapper.find('.sc-search-input > input').at(0).props().placeholder).toEqual('sample-placeholder')
 
-        expect(wrapper.find('.sc-search-input').length).toBe(1)
+    })
+    
+    it('should interact correctly', () => {
+        const mockFunc = jest.fn();
+        const props = {
+            onChange: mockFunc
+        };
+        const wrapper = shallow(<SearchInput {...props} />)
+        wrapper.find('input').simulate('change', { target: { value: 'a' } });
+        expect(mockFunc.mock.calls.length).toBe(1);
+
+        wrapper.find('input').simulate('change', { target: { value: 'ab' } });
+        expect(mockFunc.mock.calls.length).toBe(2);
+
+        wrapper.find('input').simulate('change', { target: { value: 'abc' } });
+        expect(mockFunc.mock.calls.length).toBe(3);
     })
 });
