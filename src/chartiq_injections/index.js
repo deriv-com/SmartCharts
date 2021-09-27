f86c74bd55923d6acaee51d4ee01238b4ea4f117
maryia-deriv <maryia@deriv.com>
2021-09-27 11:45:25 +0300

    Maryia/Avoiding error in case this.p0 or this.p1 is undefined or null

--- src/chartiq_injections/index.js
+++ src/chartiq_injections/index.js
@@ -2,7 +2,11 @@ import { createXAxis } from './createXAxis';
 import { drawCurrentPriceLine } from './currentHR';
 import { maintainHeadsUpHR } from './headsUpHR';
 import { plotterDrawText } from './plotterDrawText';
+import { overrideRenderEllipse } from './renderEllipse';
+import { overrideRenderChannel } from './renderChannel';
+import { overrideRenderGartley } from './renderGartley';
 import { overideMeasure } from './setMeasure';
+import { overrideRenderPitchfork } from './renderPitchfork';
 import { BackingStore } from './backingStore';
 
 const inject = option => {
@@ -10,7 +14,11 @@ const inject = option => {
     drawCurrentPriceLine();
     maintainHeadsUpHR();
     plotterDrawText();
+    overrideRenderEllipse();
+    overrideRenderChannel();
+    overrideRenderGartley();
     overideMeasure(option);
+    overrideRenderPitchfork();
     BackingStore();
 };
 
