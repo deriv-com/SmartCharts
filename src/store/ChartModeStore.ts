d39fa9446080777d5af15279af737c08a0e3f053
maryia-deriv <maryia@deriv.com>
2021-11-09 21:56:06 +0300

    Maryia/added types to Menu and ChartModeStore

--- src/store/ChartModeStore.ts
+++ src/store/ChartModeStore.ts
@@ -1,13 +1,15 @@
 import MenuStore from './MenuStore';
-import Menu from '../components/Menu';
+import Menu, { TMenuProps } from '../components/Menu';
+import { TMainStore } from '../types';
+import { TReactComponent } from './Connect';
 
 export default class ChartModeStore {
-    ChartTypeMenu: any;
-    mainStore: any;
-    menu: any;
-    constructor(mainStore: any) {
+    ChartTypeMenu: React.ReactNode;
+    mainStore: TMainStore;
+    menu: MenuStore;
+    constructor(mainStore: TMainStore) {
         this.mainStore = mainStore;
         this.menu = new MenuStore(mainStore, { route: 'chart-mode' });
-        this.ChartTypeMenu = this.menu.connect(Menu);
+        this.ChartTypeMenu = this.menu.connect(Menu as TReactComponent<TMenuProps>);
     }
 }
