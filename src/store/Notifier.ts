1be73e9beb5f7edce1621048ff7a56bf2f3c5844
maryia-deriv <maryia@deriv.com>
2021-11-23 12:44:38 +0300

    Maryia/added types to Notifier

--- src/store/Notifier.ts
+++ src/store/Notifier.ts
@@ -1,8 +1,14 @@
+export type TNotification = {
+    text: string;
+    type?: 'info' | 'warning' | 'success' | 'error' | 'warning';
+    category: string;
+};
+
 class Notifier {
     static get CATEGORY_ACTIVE_SYMBOL() {
         return 'activesymbol';
     }
-    onMessage: any;
+    onMessage?: (message: TNotification) => void;
 
     /*
      * a Notification object has the following structure:
@@ -12,7 +18,7 @@ class Notifier {
      *     category, // choose from `activesymbol`
      * }
      */
-    notify(notification: any) {
+    notify(notification: TNotification) {
         if (this.onMessage) {
             this.onMessage({
                 type: 'warning',
@@ -21,7 +27,7 @@ class Notifier {
         }
     }
 
-    notifyMarketOpen(symbol: any) {
+    notifyMarketOpen(symbol: string) {
         this.notify({
             text: t.translate('[symbol] market is now opened.', { symbol }),
             type: 'info',
@@ -29,21 +35,21 @@ class Notifier {
         });
     }
 
-    notifyMarketClose(symbol: any) {
+    notifyMarketClose(symbol: string) {
         this.notify({
             text: t.translate('[symbol] market is presently closed.', { symbol }),
             category: Notifier.CATEGORY_ACTIVE_SYMBOL,
         });
     }
 
-    notifyDelayedMarket(symbol: any, delay: any) {
+    notifyDelayedMarket(symbol: string, delay: number) {
         this.notify({
             text: t.translate('[symbol] feed is delayed by [delay] minutes', { symbol, delay }),
             category: Notifier.CATEGORY_ACTIVE_SYMBOL,
         });
     }
 
-    notifyFeedUnavailable(symbol: any) {
+    notifyFeedUnavailable(symbol: string) {
         this.notify({
             text: t.translate('Streaming for [symbol] is not available due to license restrictions', { symbol }),
             type: 'error',
