048ba6d71824e2ada36de47a57a3a6ab12e24345
amina-deriv <amina@deriv.com>
2021-11-23 10:33:07 +0400

    stx

--- src/store/DrawToolsStore.js
+++ src/store/DrawToolsStore.js
@@ -59,7 +59,7 @@ export default class DrawToolsStore {
     closeOnEscape = e => {
         const ESCAPE = 27;
         if (e.keyCode === ESCAPE) {
-            this.stx.changeVectorType('');
+            this.stx?.changeVectorType('');
             this.drawingFinished();
         }
     };
