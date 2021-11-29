5d848ff2d0306ff088c6ed88b425be472072a588
maryia-deriv <maryia@deriv.com>
2021-11-29 17:28:07 +0300

    Maryia/replaced direct change in standard.js with injection to avoid error if myPanel is null

--- src/chartiq_injections/index.js
+++ src/chartiq_injections/index.js
@@ -3,6 +3,7 @@ import { drawCurrentPriceLine } from './currentHR';
 import { maintainHeadsUpHR } from './headsUpHR';
 import { plotterDrawText } from './plotterDrawText';
 import { overrideCalculateAwesome } from './calculateAwesome';
+import { overrideFindHighlights } from './findHighlights';
 import { overideMeasure } from './setMeasure';
 import { overrideRenderEllipse } from './renderEllipse';
 import { overrideRenderChannel } from './renderChannel';
@@ -19,6 +20,7 @@ const inject = option => {
     maintainHeadsUpHR();
     plotterDrawText();
     overrideCalculateAwesome();
+    overrideFindHighlights();
     overideMeasure(option);
     overrideRenderEllipse();
     overrideRenderChannel();
