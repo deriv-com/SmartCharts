020532e124ee430ad9c1076782b1a9331c6d126f
Maryia <87321516+maryia-deriv@users.noreply.github.com>
2022-01-07 07:11:40 +0300

    Maryia/replaced direct change in standard.js with injection to avoid error if myPanel is null (#1230)
    Co-authored-by: Bala <bala.krishna@regentmarkets.com>

--- chartiq/development/js/standard.js
+++ chartiq/development/js/standard.js
@@ -7566,7 +7566,7 @@ CIQ.ChartEngine.prototype.findHighlights = function (isTap, clearOnly) {
 		this.setMeasure();
 	}
 
-	if (highlightedDraggable && myPanel && !myPanel.noDrag) {
+	if (highlightedDraggable && !myPanel.noDrag) {
 		if (this.longHoldTookEffect && !this.cancelLongHold) {
 			if (highlightedDraggable.params) {
 				// series, highlight relatives
--- src/chartiq_injections/index.ts
+++ src/chartiq_injections/index.ts
@@ -4,6 +4,7 @@ import { drawCurrentPriceLine } from './currentHR';
 import { maintainHeadsUpHR } from './headsUpHR';
 import { plotterDrawText } from './plotterDrawText';
 import { overrideCalculateAwesome } from './calculateAwesome';
+import { overrideFindHighlights } from './findHighlights';
 import { overideMeasure } from './setMeasure';
 import { overrideRenderEllipse } from './renderEllipse';
 import { overrideRenderChannel } from './renderChannel';
@@ -20,6 +21,7 @@ const inject = (option: { drawToolsStore: DrawToolsStore }) => {
     maintainHeadsUpHR();
     plotterDrawText();
     overrideCalculateAwesome();
+    overrideFindHighlights();
     overideMeasure(option);
     overrideRenderEllipse();
     overrideRenderChannel();
