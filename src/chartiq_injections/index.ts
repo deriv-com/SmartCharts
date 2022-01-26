139c113c691e44dbe9df52b71f80b517cec04228
Maryia Frantsava <88426229+maryiafrantsava-deriv@users.noreply.github.com>
2022-01-26 06:14:53 +0300

    MaryiaF/fix: TrackJS error this.controls.crossX.classList.replace is … (#1236)
    * MaryiaF/fix: TrackJS error this.controls.crossX.classList.replace is not a function
    
    * MaryiaF/refactor: space between keywords conditions
    
    * MaryiaF/fix: for case controls.crossX.classList is not a string
    
    * modify file to TS
    
    Co-authored-by: Bala <bala.krishna@regentmarkets.com>
    Co-authored-by: balakrishna-binary <56330681+balakrishna-binary@users.noreply.github.com>

--- src/chartiq_injections/index.ts
+++ src/chartiq_injections/index.ts
@@ -14,6 +14,7 @@ import { overrideRenderRectangle } from './renderRectangle';
 import { overrideRenderSegment } from './renderSegment';
 import { overrideResizeObserver } from './resizeObserver';
 import { BackingStore } from './backingStore';
+import { overrideDrawingClickChartEngine } from './drawingClickChartEngine';
 
 const inject = (option: { drawToolsStore: DrawToolsStore }) => {
     createXAxis();
@@ -31,6 +32,7 @@ const inject = (option: { drawToolsStore: DrawToolsStore }) => {
     overrideRenderSegment();
     overrideResizeObserver();
     BackingStore();
+    overrideDrawingClickChartEngine();
 };
 
 export default inject;
