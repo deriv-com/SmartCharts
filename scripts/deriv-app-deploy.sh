1bff1137ab98c1a36693f7497c32814199bc55ca
Bala <bala.krishna@regentmarkets.com>
2022-06-24 16:33:02 +0800

    chore: inspect

--- scripts/deriv-app-deploy.sh
+++ scripts/deriv-app-deploy.sh
@@ -2,4 +2,7 @@ mkdir SmartCharts && find ./ -and -not -path './SmartCharts*' -and -not -path '.
 git clone https://github.com/binary-com/deriv-app.git --depth 1
 cd deriv-app && npm run bootstrap && rm -rf node_modules/@deriv/deriv-charts/dist
 cd ../SmartCharts && npm run build -- --output-path "../deriv-app/node_modules/@deriv/deriv-charts/dist"
+ls -la ../deriv-app/node_modules/@deriv/
+ls -la ../deriv-app/node_modules/@deriv/deriv-charts
+ls -la ../deriv-app/node_modules/@deriv/deriv-charts/dist
 cd ../deriv-app && npm run build:prod
