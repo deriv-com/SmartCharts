87e44dd73c72b5c2b8d9a67d37bbeae7c20f9427
Bala <bala.krishna@regentmarkets.com>
2022-06-24 16:02:38 +0800

    chore: fix 5

--- scripts/deriv-app-deploy.sh
+++ scripts/deriv-app-deploy.sh
@@ -1,4 +1,4 @@
-mkdir SmartCharts && find ./ -and -not -path './SmartCharts*' -and -not -path './scripts*' -and -not -path './.git*' -exec mv {} ./SmartCharts \;
+mkdir SmartCharts && find ./ -and -not -path './SmartCharts*' -and -not -path './.git*' -exec mv {} ./SmartCharts \;
 git clone https://github.com/binary-com/deriv-app.git
 cd deriv-app && npm run bootstrap && rm -rf node_modules/@deriv/deriv-charts/dist
 cd ../SmartCharts && npm run build -- --output-path "../deriv-app/node_modules/@deriv/deriv-charts/dist"
