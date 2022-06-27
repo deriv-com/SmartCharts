3b9b101bc46f4790400e66bc32bf179ef16d1b36
Bala <bala.krishna@regentmarkets.com>
2022-06-27 12:07:02 +0800

    chore: fix cd

--- scripts/deriv-app-deploy.sh
+++ scripts/deriv-app-deploy.sh
@@ -1,5 +1,5 @@
 npm run build
 git clone https://github.com/binary-com/deriv-app.git --depth 1
-cd deriv-app && npm run bootstrap && rm -rf node_modules/@deriv/deriv-charts/dist && cd..
+cd deriv-app && npm run bootstrap && rm -rf node_modules/@deriv/deriv-charts/dist && cd ..
 cp -R dist deriv-app/node_modules/@deriv/deriv-charts/
 cd deriv-app && npm run build:prod
