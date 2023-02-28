bd0bb24256f8ffcda57d85574c3b07083bc4eda2
HENRY <henry@regentmarkets.com>
2023-02-28 13:54:15 +0800

    fix: english language flag trigger condition fixed

--- src/components/categoricaldisplay/ResultsPanel.tsx
+++ src/components/categoricaldisplay/ResultsPanel.tsx
@@ -150,7 +150,7 @@ const redirectLink = (subCategoryId: string, is_eu_client: boolean) => {
     const add_EU_suffix = is_eu_client ? 'eu.' : '';
     const link_mapper = is_eu_client ? eu_subcategory_mapper[subCategoryId] : row_subcategory_mapper[subCategoryId];
     let language = `${lang_from_url}/`;
-    if (is_eu_client && lang_from_url === 'EN') language = '';
+    if (is_eu_client && lang_from_url === 'en') language = '';
     let link = `https://${add_EU_suffix}deriv.com/${language}`;
     if (link_mapper) link += `markets/${link_mapper}/`;
     return link;
