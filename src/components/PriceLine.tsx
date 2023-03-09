d4a851f004b4e5c83534c9487d3d7989f87befff
kate-deriv <kate@regentmarkets.com>
2023-03-09 14:21:32 +0300

    refactor: remove ternary operator

--- src/components/PriceLine.tsx
+++ src/components/PriceLine.tsx
@@ -65,18 +65,12 @@ const PriceLine = ({
                 })}
                 style={{
                     color: foregroundColor,
-                    backgroundImage: lineStyle ? '' : `linear-gradient(to left, ${color} 90%, ${color}00`,
+                    backgroundImage:
+                        lineStyle && lineStyle !== 'solid' ? '' : `linear-gradient(to left, ${color} 90%, ${color}00`,
                 }}
             >
                 {showBarrierDragLine && (
-                    <div
-                        className='drag-line'
-                        style={
-                            lineStyle
-                                ? { borderTop: `${lineStyle} ${color} 1px` }
-                                : { borderTopStyle: lineStyle as 'solid' | 'dotted' }
-                        }
-                    />
+                    <div className='drag-line' style={{ borderTop: `${lineStyle} ${color} 1px` }} />
                 )}
                 <div className='draggable-area' />
                 <div className='drag-price' style={{ backgroundColor: color, width, opacity }}>
