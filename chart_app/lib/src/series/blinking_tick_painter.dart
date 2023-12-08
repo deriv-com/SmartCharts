c57716e77d46c409561926a933669c3368ac46ab
ahmadtaimoor-deriv <129935294+ahmadtaimoor-deriv@users.noreply.github.com>
2023-12-08 17:55:05 +0800

    adding highlighted blink

--- chart_app/lib/src/series/blinking_tick_painter.dart
+++ chart_app/lib/src/series/blinking_tick_painter.dart
@@ -1,14 +1,15 @@
 import 'package:chart_app/src/series/blinking_tick_indicator.dart';
 import 'package:deriv_chart/deriv_chart.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 ///A class for Tick Painter.
 class BlinkingTickPainter<T extends BlinkingTickIndicator>
-    extends SeriesPainter<T> {
+    extends HorizontalBarrierPainter<T> {
   /// Initializes [series].
   BlinkingTickPainter(T series) : super(series);
 
-  ///
+  /// Paint
   late Paint _paint;
 
   @override
@@ -23,11 +24,53 @@ class BlinkingTickPainter<T extends BlinkingTickIndicator>
       ..style = PaintingStyle.fill
       ..strokeWidth = 4
       ..color = Colors.redAccent;
-    print('paint - ${DateTime.now().second}');
+
+    /// Animated Value
+    double? animatedValue;
+
+    /// Epoch X
+    double? dotX;
+
+    if (series.previousObject == null) {
+      animatedValue = series.value;
+      if (series.epoch != null) {
+        dotX = epochToX(series.epoch!);
+      }
+    } else {
+      final BarrierObject previousBarrier = series.previousObject!;
+      animatedValue = series.value;
+
+      animatedValue = ui.lerpDouble(
+        previousBarrier.value,
+        series.value,
+        animationInfo.currentTickPercent,
+      );
+
+      if (series.epoch != null && series.previousObject!.leftEpoch != null) {
+        dotX = ui.lerpDouble(
+          epochToX(series.previousObject!.leftEpoch!),
+          epochToX(series.epoch!),
+          animationInfo.currentTickPercent,
+        );
+      }
+    }
+
+    final double y = quoteToY(animatedValue!);
+
+    // print('current tick percent - ${animationInfo.currentTickPercent}');
+    // print('paint - ${DateTime.now().second}');
+
+    if (animationInfo.currentTickPercent > 0.85) {
+      canvas.drawCircle(
+        Offset(dotX as double, y),
+        animationInfo.currentTickPercent * 9,
+        Paint()..color = _paint.color.withOpacity(0.2),
+      );
+    }
 
     canvas.drawCircle(
-      Offset(epochToX(series.epoch as int), quoteToY(series.value as double)),
-      _paint.strokeWidth * animationInfo.currentTickPercent,
+      Offset(dotX as double, y),
+      4,
       Paint()..color = _paint.color,
     );
   }
