382390fa9be75fe9889f3ecfbf4e990248ec5240
Farhan Ahmad Nurzi <farhan.nurzi@regentmarkets.com>
2024-03-19 15:20:54 +0800

    feat: release automation

---
+++ release.config.cjs
@@ -0,0 +1,72 @@
+const transform = require('./release.utils.cjs');
+
+module.exports = {
+    branches: [
+        '+([0-9])?(.{+([0-9]),x}).x',
+        'main',
+        'next',
+        'next-major',
+        { name: 'beta', prerelease: true },
+        { name: 'alpha', prerelease: true },
+    ],
+    repositoryUrl: 'git@github.com:binary-com/SmartCharts.git',
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                releaseRules: [
+                    {
+                        type: 'feat',
+                        release: 'minor',
+                    },
+                    {
+                        type: 'build',
+                        release: 'patch',
+                    },
+                    {
+                        type: 'ci',
+                        release: 'patch',
+                    },
+                    {
+                        type: 'chore',
+                        release: 'patch',
+                    },
+                    {
+                        type: 'docs',
+                        release: 'patch',
+                    },
+                    {
+                        type: 'refactor',
+                        release: 'patch',
+                    },
+                    {
+                        type: 'style',
+                        release: 'patch',
+                    },
+                    {
+                        type: 'test',
+                        release: 'patch',
+                    },
+                ],
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                parserOpts: {
+                    mergePattern: /^Merge pull request #(\d+) from (.*)$/,
+                    mergeCorrespondence: ['id', 'source'],
+                },
+                writerOpts: { transform },
+            },
+        ],
+        '@semantic-release/changelog',
+        [
+            '@semantic-release/npm',
+            {
+                npmPublish: true,
+            },
+        ],
+        '@semantic-release/github',
+    ],
+};
