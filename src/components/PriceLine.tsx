61d48f71a6255dbcd77f1173c0cf3ae99fe2cfcd
kate-deriv <kate@regentmarkets.com>
2024-11-18 09:24:09 +0300

    chore: remove border radius

--- src/components/PriceLine.tsx
+++ src/components/PriceLine.tsx
@@ -96,8 +96,6 @@ const PriceLine = ({
                             width: draggable && isOverlappingWithPriceLine ? width + 6 : width - 6,
                             opacity,
                             right: price_right_offset,
-                            borderTopRightRadius: isOverlappingWithPriceLine ? 0 : 4,
-                            borderBottomRightRadius: isOverlappingWithPriceLine ? 0 : 4,
                         }}
                     >
                         <HamburgerDragIcon />
